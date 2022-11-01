67c465da63b362760d5d8a216404d9f8fe345df4
Ahlul <ahlul wall aqdy>
2022-11-01 09:45:07 +0700

    selesai

---
+++ praktikum-02-01-variabel-dan-tipe-data/variabel.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * ini adalah nilai awal variabel
+ */
+$nama = "pasnur";
+$usia = 42;
+echo"<p>Hai,perkenalkan nama saya adalah",$nama,",dan saya berusia".$usia." tahun>";
+/**
+ * Ini adalah variabel dengan nilai yang di ubah
+ */
+$nama = Ahlul wall aqdy;
+$usia = 21;
+echo "<p>Hai,perkenalkan nama saya adalah ",$nama,",dan saya berusia",$usia." tahun<p>";
+?>
