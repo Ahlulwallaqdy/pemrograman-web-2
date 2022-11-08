a9acb37c19d1afdbf534a90f18a9b10478ab131b
Ahlul <ahlul wall aqdy>
2022-11-08 09:49:18 +0700

    sdh

--- praktikum-02-01-variabel-dan-tipe-data/variabel.php
+++ praktikum-02-01-variabel-dan-tipe-data/variabel.php
@@ -1,14 +1,13 @@
 <?php
-/**
- * ini adalah nilai awal variabel
- */
-$nama = "pasnur";
-$usia = 42;
-echo"<p>Hai,perkenalkan nama saya adalah",$nama,",dan saya berusia".$usia." tahun>";
-/**
- * Ini adalah variabel dengan nilai yang di ubah
- */
-$nama = Ahlul wall aqdy;
+//ini adalah nilai awal variabel
+
+$nama = "Pasnur";
+$usia = 41;
+echo "<p>Hai, perkenalkan nama saya adalah ".$nama.", dan saya berusia ".$usia." tahun<p>";
+
+//ini adalah variabel dengan nilai yang di ubah
+$nama = "Ahlul Wall Aqdy";
 $usia = 21;
-echo "<p>Hai,perkenalkan nama saya adalah ",$nama,",dan saya berusia",$usia." tahun<p>";
+echo "<p>Hai, perkenalkan nama saya adalah ".$nama.", dan saya berusia ".$usia." tahun<p>";
+
 ?>
